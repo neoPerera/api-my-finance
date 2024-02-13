0dc6ee62d156bc16e25f833a776023383414b82c
Chanuth Nilushan <chanuthnilushan@gmail.com>
2024-02-13 20:25:55 +0530

    Update cron-jobRoute.js
    Tests

--- routes/jobs/cron-jobRoute.js
+++ routes/jobs/cron-jobRoute.js
@@ -14,8 +14,14 @@ router.post("/", routeFunction);
 
 function routeFunction(req, res) {
    console.log("Cron job started");
+   try{
+    const { username, password } = req.body;
+    res.status(200).json({ message: "Notification successful" });
+   }
+   catch(error){
+    res.status(500).json({ message: "CronJOB unsuccessful" });
+   }
   // socket.disconnect()
-  res.status(200).json({ message: "Notification successful" });
 }
 
 module.exports = router;
