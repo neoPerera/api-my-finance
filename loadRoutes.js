483402f5f4b2ac0cebcc20da24554e5caf2c2b0e
Chanuth Nilushan <chanuthnilushan@gmail.com>
2024-02-21 20:31:08 +0530

    email related changes

--- loadRoutes.js
+++ loadRoutes.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const logger = require('./Helpers/logger');
 
 const exploreDirectory = (dir, callback) => {
   const files = fs.readdirSync(dir);
@@ -21,20 +22,25 @@ const loadRoutes = (app) => {
 
   exploreDirectory(routersDir, (file, relativePath) => {
     try {
-      const folderName = file.split(".")[0];
-      const routerPath = path.join(routersDir, relativePath);
-      const dirname = path.dirname(relativePath);
-      const routename = path.relative(routersDir, dirname).split("..",2)[1].replace(/\\/g, '/')|| "";
-      // Now you can use folderName, routerPath, and relativePath as needed
-      // console.log("File:", file);
-      // console.log("Folder Name:", folderName);
-      // console.log("Router Path:", routerPath);
-      // console.log("Relative Path:", relativePath);
-      console.log("url Path:",`api${routename}` );
+      // console.log(path.extname(file));
+      if(path.extname(file) == '.js')
+      {
 
-      // Load the router if needed
-      const router = require(routerPath);
-      app.use(`/api${routename}`, router);
+        const folderName = file.split(".")[0];
+        const routerPath = path.join(routersDir, relativePath);
+        const dirname = path.dirname(relativePath);
+        const routename = path.relative(routersDir, dirname).split("..",2)[1].replace(/\\/g, '/')|| "";
+        // Now you can use folderName, routerPath, and relativePath as needed
+        // console.log("File:", file);
+        // console.log("Folder Name:", folderName);
+        // console.log("Router Path:", routerPath);
+        // console.log("Relative Path:", relativePath);
+        console.log(`url Path:api${routename}` );
+        
+        // Load the router if needed
+        const router = require(routerPath);
+        app.use(`/api${routename}`, router);
+      }
 
     } catch (error) {
       console.error(`Error loading router from file: ${file}`, error);
--- routes/test/testRoute.js
+++ routes/test/testRoute.js
@@ -1,6 +1,8 @@
 // homeRoute.js
 const express = require("express");
 const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
+
 const pool = require("./../../Helpers/databaseHelper");
 const {
   getMessaging,
@@ -15,9 +17,19 @@ require("dotenv").config();
 const router = express.Router();
 router.use(bodyParser.json());
 
+const transporter = nodemailer.createTransport({
+  host: 'smtp.zoho.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'me@chanuthperera.com',
+    pass: 'n30p3r3r4@zoho.COM',
+  },
+});
+
 router.get("/", routeFunction);
 
-function routeFunction(req, res) {
+async function routeFunction(req, res) {
   // const registrationToken = "dnOHtv7GOQhniSivYIblYV:APA91bFAQOF25-nryKa3LjDT1MhwaG6bXhLBQ0iC7RMZA8OKyxiRLHT7GSPGP7mzRq8iAMjF59F9C_wHNb4GSEKT9fQbJKSauecoS-v_3zVAGO1WoACxWogA2Se-1KC-yAQj9uQWxQPH";
 
   // const message = {
@@ -40,15 +52,32 @@ function routeFunction(req, res) {
   //     console.log("Error sending message:", error);
   //   });
 
-  let socket = io(process.env.NOTIFICATION_API);
-  socket.emit("notification", {
-    to_user: "chanuth",
-    data: { title: "This is a test notification", message: "test" },
-  });
-  socket.on('notification',(props)=>{
-    console.log("GOT MESSAGE==>"+ props);
-  });
+  // let socket = io(process.env.NOTIFICATION_API);
+  // socket.emit("notification", {
+  //   to_user: "chanuth",
+  //   data: { title: "This is a test notification", message: "test" },
+  // });
+  // socket.on('notification',(props)=>{
+  //   console.log("GOT MESSAGE==>"+ props);
+  // });
   // socket.disconnect()
+
+  const mailOptions = {
+    from: 'me@chanuthperera.com',
+    to: 'chanuthnilushan@gmail.com',
+    subject: 'test',
+    text: 'text',
+    attachments: [
+      {
+        filename: 'CHA05214.JPG', // Change the filename as needed
+        path: 'C:/Users/Chanuthp/Desktop/CHA05214.JPG', // Provide the correct path to your file
+        // You can also use a Buffer or a stream instead of a file path
+      },
+    ],
+  };
+
+  // Send email
+  await transporter.sendMail(mailOptions);
   res.status(200).json({ message: "Notification successful" });
 }
 
